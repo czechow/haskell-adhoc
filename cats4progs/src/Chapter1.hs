f1cf3de16b31ca071611c751503377ac1c149f77
czechow <pawel.czechowicz@vp.pl>
2016-02-11 23:45:49 +0100

    Chapter 1 examples

---
+++ cats4progs/src/Chapter1.hs
@@ -0,0 +1,20 @@
+module Chapter1
+    ( myId
+    , myComp
+    , testMyComp
+    ) where
+
+-- Challenge 1
+myId :: a -> a
+myId x = x
+
+-- Challenge 2
+myComp :: (b -> c) -> (a -> b) -> (a -> c)
+myComp g f = \x -> g (f x)
+
+-- Challenge 3
+testMyComp :: Bool
+testMyComp = (filter (not . id) $
+              map (\x -> myComp f myId x == myComp myId f x) [1 :: Int ..100])
+             == []
+  where f x = x * x
