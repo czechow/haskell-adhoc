25e0dc448926d51adfc73b40c4cd20f57797a162
czechow <pawel.czechowicz@vp.pl>
2016-02-12 21:26:45 +0100

    One can do that more easily

--- cats4progs/src/Chapter1.hs
+++ cats4progs/src/Chapter1.hs
@@ -14,7 +14,6 @@ myComp g f = \x -> g (f x)
 
 -- Challenge 3
 testMyComp :: Bool
-testMyComp = (filter (not . id) $
-              map (\x -> myComp f myId x == myComp myId f x) [1 :: Int ..100])
-             == []
+testMyComp =
+  and $ map (\x -> myComp f myId x == myComp myId f x) [1 :: Int ..100]
   where f x = x * x
