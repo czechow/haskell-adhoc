dea69631c61ab14f0e1eec16ed7467cf0101ad95
czechow <pawel.czechowicz@vp.pl>
2016-02-16 22:48:51 +0100

    New channel implementation

--- conc/src/Channel.hs
+++ conc/src/Channel.hs
@@ -3,6 +3,32 @@ module Channel where
 import Control.Concurrent
 
 
+type Stream a = MVar (Item a)
+data Item a = Item a (Stream a)
+
+data Ch a = Ch (MVar (Stream a)) (MVar (Stream a))
+
+newCh :: IO (Ch a)
+newCh = do
+  emptyStream <- newEmptyMVar
+  mvRead <- newMVar emptyStream
+  mvWrite <- newMVar emptyStream
+  return $ Ch mvRead mvWrite
+
+writeCh :: Ch a -> a -> IO ()
+writeCh (Ch _ mvWrite) x = do
+  currStream <- takeMVar mvWrite
+  emptyStream <- newEmptyMVar
+  putMVar currStream $ Item x emptyStream
+  putMVar mvWrite emptyStream
+
+readCh :: Ch a -> IO a
+readCh (Ch mvRead _) = do
+  currStream <- takeMVar mvRead
+  (Item x nextStream) <- readMVar currStream
+  putMVar mvRead nextStream
+  return x
+
 -- we want to implement channels that allow simultaneous read and write
 
 data Channel a = Channel (MVar (MVar (Node a))) (MVar (MVar (Node a)))
