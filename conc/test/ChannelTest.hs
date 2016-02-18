68e1772bbd74752c9e924253488a9c5c3e2c2050
czechow <pawel.czechowicz@vp.pl>
2016-02-19 00:21:01 +0100

    Monadic testing added

--- conc/test/ChannelTest.hs
+++ conc/test/ChannelTest.hs
@@ -3,15 +3,15 @@
 module ChannelTest where
 
 import Test.Tasty
---import Test.Tasty.TH
 import Test.Tasty.QuickCheck
 import Test.QuickCheck.Monadic
---import Test.QuickCheck
 
 import System.Process
-
+import Control.Concurrent
 import Channel
 
+-- For more examples on monadic testing see
+-- https://hackage.haskell.org/package/QuickCheck-2.8.2/docs/Test-QuickCheck-Monadic.html#v:run
 
 class EOFy a where
   eof :: a
@@ -19,38 +19,23 @@ class EOFy a where
 instance EOFy Int where
   eof = -1
 
+ms:: Integer -> Integer
+ms = (*1000)
 
 test_transfer :: [TestTree]
 test_transfer =
-  [ testProperty "all data get through channel and in the same order" $
-    prop_factor
+  [ localOption (mkTimeout $ ms 1000) $
+    testProperty "all data get through channel and in the same order" $
+    testTransferOneThread
   ]
 
--- $ factor 16
--- 16: 2 2 2 2
-factor :: Integer -> IO [Integer]
-factor n = parse `fmap` readProcess "factor" [show n] "" where
-
-  parse :: String -> [Integer]
-  parse = map read . tail . words
-
-prop_factor :: Positive Integer -> Property
-prop_factor (Positive n) = monadicIO $ do
-  factors <- run (factor n)
-  assert (product factors == n)
-
-test_props :: [TestTree]
-test_props =
-  [ testProperty "XXX" $
-    \a b -> a == (5 :: Int) && b == (6 :: Int)
-  , testProperty "" $ monadicIO $ do
-      assert False
-  ]
-
-
-writerFunction :: Ch a -> [a] -> IO ()
-writerFunction channel xs = mapM_ (writeCh channel) xs
+testTransferOneThread:: [Int] -> Property
+testTransferOneThread xs = monadicIO $ do
+  ch <- run $ newCh
+  _ <- run $ forkIO $ sequence_ [writeCh ch x | x <- xs]
+  zs <- run $ sequence [readCh ch | _ <- xs]
+  assert (zs == xs)
 
 
 tests :: TestTree
-tests = testGroup "All" (test_transfer ++ test_props)
+tests = testGroup "ChannelTests" (test_transfer)
