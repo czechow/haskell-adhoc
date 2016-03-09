e949c81be828d8d1c9f2807adb66d910e853e2a9
czechow <pawel.czechowicz@vp.pl>
2016-03-09 22:31:41 +0100

    Arrows again

--- gravity/app/Main.hs
+++ gravity/app/Main.hs
@@ -4,27 +4,48 @@ module Main where
 
 import Text.XML.HXT.Core
 
+-- data Guest = Guest { firstName :: String
+--                    , lastName :: String }
+--            deriving (Show, Eq)
+
 -- main :: IO ()
 -- main = do
---   xml <- readFile "s1.xml"
---   let doc = readString [withWarnings yes] xml
---   res <- runX . xshow $ doc >>> indentDoc
---   putStrLn "Up and running"
---   mapM_ putStrLn res
+--   guests <- runX (readDocument [ withValidate no] "s3.xml"
+--                   >>> getGuest)
+--   print guests
 
-data Guest = Guest { firstName :: String
-                   , lastName :: String }
-           deriving (Show, Eq)
+
+-- getGuest = deep (isElem >>> hasName "guest") >>>
+--   proc x -> do
+--     fname <- getText <<< getChildren <<< deep (hasName "fname" <<< isElem) -< x
+--     lname <- getText <<< getChildren <<< deep (hasName "lname") -< x
+--     returnA -< Guest { firstName = fname, lastName = lname }
+
 
 main :: IO ()
 main = do
-  guests <- runX (readDocument [ withValidate no] "s3.xml"
-                  >>> getGuest)
-  print guests
+  xml <- readFile "s2.xml"
+  let doc = readString [withWarnings yes] xml
+  eps <- runX $ doc >>> getRoot >>> getEndpoints
+  rts <- runX $ doc >>> getRoot >>> getRoutes
+  print eps
+  print rts
+  print $ length rts
 
+getRoot = deep (isElem >>> hasName "camel:camelContext")
 
-getGuest = deep (isElem >>> hasName "guest") >>>
-  proc x -> do
-    fname <- getText <<< getChildren <<< deep (hasName "fname") -< x
-    lname <- getText <<< getChildren <<< deep (hasName "lname") -< x
-    returnA -< Guest { firstName = fname, lastName = lname }
+getEndpoints = proc x -> do
+  ep <- getAttrValue "id" <<< deep (hasName "endpoint") -< x
+  returnA -< ep
+
+type Name = String
+type From = String
+
+data Route = Route { name :: Name, from :: From}
+           deriving (Show)
+
+getRoutes = proc x -> do
+  rt <- deep (hasName "route" <+> hasName "camel:route" <<< isElem) -< x
+  froms <- getAttrValue "uri" <<< deep (hasName "from") <<< returnA -< rt
+  rtn <- getAttrValue "id" <<< returnA -< rt
+  returnA -< Route rtn froms
