cedd9af94ebea456fe888bad93897ddede751c14
czechow <pawel.czechowicz@vp.pl>
2016-03-13 23:43:52 +0100

    Dot diagram generation added

--- gravity/app/Main.hs
+++ gravity/app/Main.hs
@@ -3,41 +3,7 @@
 module Main where
 
 import Text.XML.HXT.Core
-import qualified Text.XML.HXT.DOM.XmlNode as XN
-
--- data Guest = Guest { firstName :: String
---                    , lastName :: String }
---            deriving (Show, Eq)
-
--- main :: IO ()
--- main = do
---   guests <- runX (readDocument [ withValidate no] "s3.xml"
---                   >>> getGuest)
---   print guests
-
-
--- getGuest = deep (isElem >>> hasName "guest") >>>
---   proc x -> do
---     fname <- getText <<< getChildren <<< deep (hasName "fname" <<< isElem) -< x
---     lname <- getText <<< getChildren <<< deep (hasName "lname") -< x
---     returnA -< Guest { firstName = fname, lastName = lname }
-
-
-main :: IO ()
-main = do
-  xml <- readFile "s2s.xml"
-  let doc = readString [withWarnings yes] xml
-  eps <- runX $ doc >>> getRoot >>> getEndpoints
-  rts <- runX $ doc >>> getRoot >>> getRoutes
-  print eps
-  print rts
-  print $ length rts
-
-getRoot = deep (isElem >>> hasName "camel:camelContext")
-
-getEndpoints = proc x -> do
-  ep <- getAttrValue "id" <<< deep (hasName "endpoint") -< x
-  returnA -< ep
+import Data.List (nub)
 
 type Name = String
 type From = String
@@ -46,10 +12,34 @@ type To = String
 data Route = Route { name :: Name, from :: From, to :: To }
            deriving (Show)
 
-getRoutes = proc x -> do
+
+sanitize :: [Route] -> [Route]
+sanitize xs = map cleanNames xs
+  where
+    cleanNames r = r { to = (takeWhile (/=':') $ to r) }
+
+outputGraph :: [Route] -> IO ()
+outputGraph xs = do
+  putStrLn "digraph G {"
+  putStrLn "rankdir=LR"
+  mapM_ (\(f, t) -> putStrLn (f ++ " -> " ++ t)) xs'
+  putStrLn "}"
+    where
+      xs' = nub $ map ((,) <$> from <*> to) xs
+
+main :: IO ()
+main = do
+  xml <- readFile "s2.xml"
+  let doc = readString [withWarnings yes] xml
+  rts <- runX $ doc >>> getRoot >>> getRoute -- This gets me the whole struct
+
+  outputGraph $ sanitize rts
+
+getRoot = deep (isElem >>> hasName "camel:camelContext")
+
+getRoute = proc x -> do
   rt <- deep (hasName "route" <+> hasName "camel:route" <<< isElem) -< x
-  froms <- getAttrValue "uri" <<< deep (hasName "from") <<< returnA -< rt
-  tos <- deep (hasName "to") -< rt
-  rtn <- getAttrValue "id" -< rt
-  tons <- listA $ getAttrValue "uri" -< tos
-  returnA -< Route rtn froms (show tons)
+  from' <- getAttrValue "uri" <<< deep (hasName "from") <<< returnA -< rt
+  to' <- getAttrValue "uri" <<< deep (hasName "to" <+> hasName "wireTap") -< rt
+  name' <- getAttrValue "id" -< rt
+  returnA -< Route name' from' to'
