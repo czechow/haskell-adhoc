df4dd3641657042d9de14f08e1d039b15fef5b23
czechow <pawel.czechowicz@vp.pl>
2016-03-14 22:31:07 +0100

    Tiny corrections

--- gravity/app/Main.hs
+++ gravity/app/Main.hs
@@ -6,6 +6,8 @@ import Text.XML.HXT.Core
 import Data.List (nub)
 import Data.List.Utils (replace)
 
+import System.Exit
+import System.Environment
 
 type Name = String
 type From = String
@@ -19,8 +21,8 @@ sanitize :: [Route] -> [Route]
 sanitize xs = map cleanNames xs
   where
     cleanNames r = r { from = cleanFrom $ from r
-                     , to = (takeWhile (/=':') $ to r) }
-    cleanFrom = (takeWhile (/='?')) . (replace "timer://" "")
+                     , to = takeWhile (/=':') $ to r }
+    cleanFrom = takeWhile (/='?') . replace "timer://" ""
 
 outputGraph :: [Route] -> IO ()
 outputGraph xs = do
@@ -31,13 +33,24 @@ outputGraph xs = do
     where
       xs' = nub $ map ((,) <$> from <*> to) xs
 
+usage :: IO ()
+usage = do
+  progName <- getProgName
+  putStrLn $ "Usage: " ++ progName ++ " xml_file"
+
+
 main :: IO ()
 main = do
-  xml <- readFile "s2.xml"
-  let doc = readString [withWarnings yes] xml
-  rts <- runX $ doc >>> getRoot >>> getRoute -- This gets me the whole struct
-
-  outputGraph $ sanitize rts
+  args <- getArgs
+  case args of
+    (fName:_) -> do
+      xml <- readFile fName
+      let doc = readString [withWarnings yes] xml
+      rts <- runX $ doc >>> getRoot >>> getRoute
+      outputGraph $ sanitize rts
+    _ -> do
+      usage
+      exitFailure
 
 getRoot = deep (isElem >>> hasName "camel:camelContext")
 
@@ -45,6 +58,6 @@ getRoute = proc x -> do
   rt <- deep (hasName "route" <+> hasName "camel:route" <<< isElem) -< x
   from' <- getAttrValue "uri" <<< deep (hasName "from") <<< returnA -< rt
   to' <- getAttrValue "uri" <<<
-         deep (hasName "camel:to" <+> hasName "to" <+> hasName "wireTap") -< rt
+         deep (hasName "camel:to" <+> hasName "wireTap" <+> hasName "to") -< rt
   name' <- getAttrValue "id" -< rt
   returnA -< Route name' from' to'
