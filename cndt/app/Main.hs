c002794fee7bb0250618fc962dc598ea2a560b4d
czechow <pawel.czechowicz@vp.pl>
2016-03-16 22:28:53 +0100

    Conduit (continued)

--- cndt/app/Main.hs
+++ cndt/app/Main.hs
@@ -9,3 +9,18 @@ main = do
 
   result <- CL.sourceList [1::Int ..10] $$ CL.fold (+) 0
   print result
+
+  putStrLn "Now pipeline"
+  source $$ conduit =$ sink
+
+
+source :: Source IO Int
+source = do
+  yield 1
+  yield 2
+
+conduit :: Conduit Int IO String
+conduit = CL.map $ show . (+3)
+
+sink :: Sink String IO ()
+sink = CL.mapM_ putStrLn
