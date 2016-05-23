852b471e55fb981e824005003823ef5db8a5e58b
czechow <pawel.czechowicz@vp.pl>
2016-05-23 23:30:09 +0200

    How slow read can be?

--- erbic/src/Httpd.hs
+++ erbic/src/Httpd.hs
@@ -2,12 +2,43 @@ module Httpd where
 
 import Network.Socket
 import Control.Concurrent
+import System.IO
+
 
 msg :: String
 msg = "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n"
 
 wt :: Int
 wt = 1
+
+data MyData = MD1 { _id :: Int
+                  , _name :: String
+                  }
+            | MD2 { _time :: Integer }
+            deriving (Show, Read)
+
+initMD :: [MyData]
+initMD = [ MD1 13 "years"
+         , MD1 997 "Police"
+         , MD2 1234
+         ]
+
+myRead :: IO ()
+myRead = do
+  h <- openFile "input.txt" ReadMode
+  cnt <- rd (0 :: Int) h
+  putStrLn $ "Lines " ++ show cnt
+  hClose h
+  return ()
+    where
+      rd i h' = do
+        eof <- hIsEOF h'
+        if eof
+          then return i
+          else do
+            line <- hGetLine h'
+            rd (i + length (read line :: [MyData])) h'
+
 
 run :: IO ()
 run = do
