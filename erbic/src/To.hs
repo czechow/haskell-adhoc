acb7aa2e64b53b1b9bb88d51c6224058c2f76f55
czechow <pawel.czechowicz@vp.pl>
2016-06-19 22:52:46 +0200

    Concurrency...

---
+++ erbic/src/To.hs
@@ -0,0 +1,61 @@
+module To where
+
+
+import System.Timeout
+import System.IO
+import Control.Concurrent
+import Control.Exception
+import Network.Socket
+import Control.Monad
+
+
+longFileRead :: Handle -> IO String
+longFileRead h = do
+  threadDelay 1000000
+  line1 <- hGetLine h
+  threadDelay 1000000
+  line2 <- hGetLine h
+  return $ line1 ++ line2
+
+run :: IO ()
+run = do
+  h <- openFile "test.txt" ReadMode
+  mto <- timeout 1500000 (longFileRead h)
+  case mto of
+    Just c -> putStrLn $ "Long read cont: [" ++ c ++ "]"
+    Nothing -> putStrLn "Timed out"
+  hGetContents h >>= \r -> (putStrLn $ "Rest: [" ++ r ++ "]")
+  hClose h
+
+type ErrMsg = String
+
+openSock :: IO (Either ErrMsg Socket)
+openSock = open `catch` handler
+  where
+    open =
+      bracketOnError
+        (socket AF_INET Stream defaultProtocol)
+        (close)
+        (\s -> do
+            setSocketOption s ReuseAddr 1
+            hostAddr <- inet_addr "127.0.0.1"
+            putStrLn $ "Acquired socket " ++ show s
+            bind s (SockAddrInet 2222 hostAddr)
+            listen s 5
+            threadDelay 3000000
+            return $ Right s)
+    handler :: SomeException -> IO (Either ErrMsg Socket)
+    handler e = do
+      putStrLn $ "Running e handler"
+      return $ Left $ "*** Exception: " ++ show e
+
+
+{-
+  readFromSocket chunk => if it fails we should be closing the socket
+
+  What we really want to have is an event driven logic processing
+  triggering loop separated from other processing with a bounded queue
+
+  s <- readFromSocket -- wrapped with either => no exceptions
+
+-}
